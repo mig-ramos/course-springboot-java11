2cb68c2c176f834b65fc44c1aa2613869de0227a
Miguel Ramos <mig.ramos10@gmail.com>
2020-11-10 00:06:31 -0300

    Project created

---
+++ src/test/java/com/uptecsi/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptecsi.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
