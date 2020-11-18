77e83a5112f5e79c1746f1bb6e67d397fa7d2f95
Miguel Ramos <mig.ramos10@gmail.com>
2020-11-17 23:45:01 -0300

    Product

---
+++ src/main/java/com/uptecsi/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.uptecsi.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uptecsi.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
