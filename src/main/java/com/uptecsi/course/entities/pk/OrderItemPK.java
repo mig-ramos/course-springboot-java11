541943917cf486780447586474968755aa771fe8
Miguel Ramos <mig.ramos10@gmail.com>
2020-11-19 21:09:47 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/uptecsi/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,37 @@
+package com.uptecsi.course.entities.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.uptecsi.course.entities.Order;
+import com.uptecsi.course.entities.Product;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
+}
