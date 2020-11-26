a7cdf8f17e1c571ffea56d69c273800c501de813
Miguel Ramos <mig.ramos10@gmail.com>
2020-11-25 23:27:47 -0300

    Exception handling - delete

--- src/main/java/com/uptecsi/course/services/UserService.java
+++ src/main/java/com/uptecsi/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.uptecsi.course.entities.User;
 import com.uptecsi.course.repositories.UserRepository;
+import com.uptecsi.course.services.exceptions.DatabaseException;
 import com.uptecsi.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+		//catch (RuntimeException e) {
+			// Só pra ver a menssagem
+			// e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id,User obj) {
